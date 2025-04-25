dd9da1a40170c09d6441e09752eee71391d54fef
dennie <denitsa.sureva@icloud.com>
2025-04-25 16:03:15 +0200

    feat:page is working

--- weatherApp/weather/views.py
+++ weatherApp/weather/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import datetime
 import requests
+from django.contrib import messages
 # Create your views here.
 def home(request):
     if "city" in request.POST:
@@ -10,19 +11,29 @@ def home(request):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=df7484304dfaba426eebf03161e567dd"
     PARAMS = {"units": "metric"} # preparing the data to be in that format
 
-    data = requests.get(url, params=PARAMS).json() # sends GET request to API, the result would be in json
 
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
+    try:
+        data = requests.get(url, params=PARAMS).json() # sends GET request to API, the result would be in json
 
-    day = datetime.date.today()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
 
-    context = {
-        "description": description,
-        "icon": icon,
-        "temp": temp,
-        "day": day,
-    }
+        day = datetime.date.today()
+
+        context = {
+            "city": city,
+            "description": description,
+            "icon": icon,
+            "temp": temp,
+            "day": day,
+            'exception_occurred': False,
+        }
+        return render(request, 'index.html', context)
+    except KeyError:
+        messages.error(request, 'Entered data is not available to API')
+        context = {
+            'exception_occurred': True,
+        }
 
     return render(request, 'index.html', context)
